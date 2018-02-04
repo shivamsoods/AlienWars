965ce68a6850ed386a88ff3741dd623d1516d0c4
sunidhi chaudhary <sunidhi.nith@gmail.com>
2018-02-04 16:07:43 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -7,18 +7,19 @@ SCREENHEIGHT=480
 HALFSCREENWIDTH=int(SCREENWIDTH/2)   #half define kar diya
 HALFSCREENHEIGHT=int(SCREENHEIGHT/2)
 
-NUM_ENEMY = 30
+NUM_ENEMY = 80
 NUM_STARS = 80
 INVULNTIME = 2
+GAMEOVERTIME = 4
 
 LEFT = 'left'
 RIGHT = 'right'
 
 MAX_HEALTH=3
 INITIAL_SIZE=30
-WIN_SIZE=300
+WIN_SIZE=230
 
-PLAYER_VELOCITY = 15
+PLAYER_VELOCITY = 20
 ENEMY_VEL_MIN=2
 ENEMY_VEL_MAX=7
 
@@ -26,22 +27,25 @@ BLACK =(0,0,0)
 WHITE=(255,255,255)
 RED = (126,100,234)
 
-CAMERAVIEW=100
+CAMERASLACK=100
+
 
 
 def main():
 
-    global PLAYERSURFACE,DISPLAYSURF,STARSURFACE,CLOCK,PLAYERSURFACE1
+    global PLAYERSURFACE,DISPLAYSURF,STARSURFACE,CLOCK,PLAYERSURFACE1,BASICFONT
 
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode([SCREENWIDTH,SCREENHEIGHT])
     pygame.display.set_caption("Game")
-   # pygame.display.set_icon(pygame.image.load("pikachu.png"))
+    pygame.display.set_icon(pygame.image.load("pikachu.png"))
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 32)
     CLOCK = pygame.time.Clock()
 
     PLAYERSURFACE=pygame.image.load("alien.png")
-    PLAYERSURFACE1=pygame.transform.flip(PLAYERSURFACE,True,False) # baad me use karne k liye phle hi flip kr liya
+    PLAYERSURFACE1=pygame.image.load("pikachu.png") # baad me use karne k liye phle hi flip kr liya
     STARSURFACE=pygame.image.load("star.png")
+
 
     while True:
         runGame()
@@ -52,16 +56,24 @@ def runGame():
     gameOverMode = False      # if the player has lost
     gameOverStartTime = 0     # time the player lost
     winMode = False           # if the player has won
+
+    gameOverSurf = BASICFONT.render('Game Over', True, WHITE)
+    gameOverRect = gameOverSurf.get_rect()
+    gameOverRect.center = (HALFSCREENWIDTH, HALFSCREENHEIGHT)
+
+    winSurf = BASICFONT.render('You have achieved OMEGA ALIEN!', True, WHITE)
+    winRect = winSurf.get_rect()
+    winRect.center = (HALFSCREENWIDTH, HALFSCREENHEIGHT)
+
     camx=0
     camy=0
-    # movex = (camx + SCREENWIDTH) / 2
-    # movey = (camy + SCREENHEIGHT) / 2
+
     playerObject = {
         "surface":PLAYERSURFACE,
         "facing":LEFT,
         "size":INITIAL_SIZE,
         "health":MAX_HEALTH,
-        "x":HALFSCREENWIDTH,   #yaha x or y ki value li
+        "x":HALFSCREENWIDTH,
         "y":HALFSCREENHEIGHT
     }
 
@@ -86,7 +98,7 @@ def runGame():
         DISPLAYSURF.fill(BLACK)  # global
        # print("hi")
         for eObj in enemies:
-            # move the squirrel
+            # move the enemies
             eObj['x'] += eObj['movex']
             eObj['y'] += eObj['movey']
 
@@ -94,10 +106,33 @@ def runGame():
         if random.randint(0, 99) < 2:
                 eObj['movex'] = getRandomVelocity()
                 eObj['movey'] = getRandomVelocity()
-                if eObj['movex'] > 0: # faces right
-                    eObj['surface'] = pygame.transform.scale(PLAYERSURFACE1, (eObj['width'], eObj['height']))
-                else: # faces left
-                    eObj['surface'] = pygame.transform.scale(PLAYERSURFACE, (eObj['width'], eObj['height']))
+
+        for i in range (len(stars)-1,-1):   #see the below one
+            if stars[i]['x'] not in range(camx-SCREENWIDTH,camx+3*SCREENWIDTH) and stars[i]['y'] not in range(camy-SCREENHEIGHT,camy+3*SCREENHEIGHT):
+                del stars[i]
+                #we have to delete the enemies as well
+        for i in range (len(enemies)-1,-1,-1):
+            if enemies[i]['x'] not in range(camx-SCREENWIDTH,camx+3*SCREENWIDTH) and enemies[i]['y'] not in range(camy-SCREENHEIGHT,camy+3*SCREENHEIGHT):
+                del enemies[i]
+
+                #append kar rahe h
+
+        while len(stars) < NUM_STARS:
+            stars.append(makeNewStar(camx,camy))
+
+        while len(enemies) < NUM_ENEMY:
+            enemies.append(makeNewEnemies(camx, camy))
+
+        playerCenterx = playerObject['x'] + int(playerObject['size'] / 2)
+        playerCentery = playerObject['y'] + int(playerObject['size'] / 2)
+        if (camx + HALFSCREENWIDTH) - playerCenterx > CAMERASLACK:
+            camx = playerCenterx + CAMERASLACK - HALFSCREENWIDTH
+        elif playerCenterx - (camx + HALFSCREENWIDTH) > CAMERASLACK:
+            camx = playerCenterx - CAMERASLACK - HALFSCREENWIDTH
+        if (camy + HALFSCREENHEIGHT) - playerCentery > CAMERASLACK:
+            camy = playerCentery + CAMERASLACK - HALFSCREENHEIGHT
+        elif playerCentery - (camy + HALFSCREENHEIGHT) > CAMERASLACK:
+            camy = playerCentery - CAMERASLACK - HALFSCREENHEIGHT
 
         for st in stars:
            #changes
@@ -105,145 +140,117 @@ def runGame():
                                 st['y']-camy,
                                 st['width'],st['height'])
           #  print(st['x'])
+
             DISPLAYSURF.blit(STARSURFACE,sRect)
          #  emnemy squuirels banayi
         for eObj in enemies:                    #yha bhi chnages kiye enemies ka rect banaya    #yaha error aa rahi h pata nahi kyu
-            # eObj['rect'] = pygame.Rect((eObj['x'] - camx,
-            #                             eObj['y'] - camy,
-            #                             eObj['width'],
-            #                             eObj['height']))
+            eObj['rect'] = pygame.Rect((eObj['x'] - camx,
+                                        eObj['y'] - camy,
+                                        eObj['width'],
+                                        eObj['height']))
+
             DISPLAYSURF.blit(eObj['surface'],(eObj['x'],eObj['y']))
             #yha pr player object ka rect banaya
+
             if not gameOverMode and not invulnerableMode:
                 playerObject['rect'] = pygame.Rect((playerObject['x']-camx,    # x and y ki value define nahi ki
                                          playerObject['y']-camy,               #thi upar isliye error aa rahi thi
                                          playerObject['size'],
                                          playerObject['size']))
+
                 DISPLAYSURF.blit(playerObject['surface'],playerObject['rect'])
-        drawHealthMeter(playerObject['health'])         #Health meter niche define kiya h
+
+            drawHealthMeter(playerObject['health'])         #Health meter niche define kiya h
 
 
-        # for event in pygame.event.get(): # event handling loop
-        #     if event.type == QUIT:
-        #         terminate()
-        #
-        #     elif event.type == KEYDOWN:
-        #         if event.key in (K_UP, K_w):
-        #             moveDown = False
-        #             moveUp = True
-        #         elif event.key in (K_DOWN, K_s):
-        #             moveUp = False
-        #             moveDown = True
-        #         elif event.key in (K_LEFT, K_a):
-        #             moveRight = False
-        #             moveLeft = True
-        #             if playerObject['facing'] != LEFT: # change player image
-        #                 playerObject['surface'] = pygame.transform.scale(PLAYERSURFACE, (playerObject['size'], playerObject['size']))
-        #             playerObject['facing'] = LEFT
-        #         elif event.key in (K_RIGHT, K_d):
-        #             moveLeft = False
-        #             moveRight = True
-        #             if playerObject['facing'] != RIGHT: # change player image
-        #                 playerObject['surface'] = pygame.transform.scale(PLAYERSURFACE1, (playerObject['size'], playerObject['size']))
-        #             playerObject['facing'] = RIGHT
-        #         elif winMode and event.key == K_r:
-        #             return
-        #
-        #     elif event.type == KEYUP:
-        #         # stop moving the player's squirrel
-        #         if event.key in (K_LEFT, K_a):
-        #             moveLeft = False
-        #         elif event.key in (K_RIGHT, K_d):
-        #             moveRight = False
-        #         elif event.key in (K_UP, K_w):
-        #             moveUp = False
-        #         elif event.key in (K_DOWN, K_s):
-        #             moveDown = False
-        #
-        #         elif event.key == K_ESCAPE:
-        #             pygame.quit()
-        #             sys.exit()
-        #
-        # if not gameOverMode:
-        #     # actually move the player
-        #     if moveLeft:
-        #         playerObject['x'] -= PLAYER_VELOCITY
-        #     if moveRight:
-        #         playerObject['x'] += PLAYER_VELOCITY
-        #     if moveUp:
-        #         playerObject['y'] -= PLAYER_VELOCITY
-        #     if moveDown:
-        #         playerObject['y'] += PLAYER_VELOCITY
-
-        for event in pygame.event.get():
+        for event in pygame.event.get(): # event handling loop
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
             elif event.type == KEYDOWN:
-                if event.key ==K_LEFT:
-                    if playerObject['facing']!=LEFT: # iski wajah se left right move nahi kar raha tha don't know why
-                       # playerObject['surface'] = pygame.transform.scale(PLAYERSURFACE,
-                                                                     # (playerObject['size'], playerObject['size']))
-                        #pygame.transform.flip(PLAYERSURFACE,False,True)  # yaha changes honge
-                        playerObject['facing']=LEFT
-                    playerObject['x'] -= PLAYER_VELOCITY
-                    # movex -= PLAYER_VELOCITY ye nahi hoga
-                    if playerObject['x'] < camx+HALFSCREENWIDTH :
-                       camx -= PLAYER_VELOCITY
-
-                elif event.key == pygame.K_RIGHT:
-                    if playerObject['facing']!=RIGHT:
-                        #pygame.transform.flip(PLAYERSURFACE,False,True)
-
-                        playerObject['facing']=RIGHT
-                    playerObject['x'] += PLAYER_VELOCITY
-                    #movex += PLAYER_VELOCITY
-                    if playerObject['x']> camx+HALFSCREENWIDTH:
-                        camx += PLAYER_VELOCITY
-
-                elif event.key == pygame.K_UP:
-                   # movey -= PLAYER_VELOCITY
-                   playerObject['y'] -= PLAYER_VELOCITY
-
-                #if movey < camy+50 : yaha 50 ki jagah halfscreenheight le li
-                if playerObject['y'] < camy+HALFSCREENHEIGHT:
-                        camy -= PLAYER_VELOCITY
-
-                elif event.key == pygame.K_DOWN:
-                    playerObject['y'] += PLAYER_VELOCITY
-                   # movey += PLAYER_VELOCITY
-
-                    if playerObject['y']> camy+HALFSCREENHEIGHT :
-                       camy += PLAYER_VELOCITY
-
-                elif event.key== pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
+                if event.key in (K_UP, K_w):
+                    moveDown = False
+                    moveUp = True
+                elif event.key in (K_DOWN, K_s):
+                    moveUp = False
+                    moveDown = True
+                elif event.key in (K_LEFT, K_a):
+                    moveRight = False
+                    moveLeft = True
+                    if playerObject['x'] < camx + HALFSCREENWIDTH:
+                        camx -= PLAYER_VELOCITY
+                    #if playerObject['facing'] != LEFT: # change player image
+                     #   playerObject['surface'] = pygame.transform.scale(PLAYERSURFACE, (playerObject['size'], playerObject['size']))
+                    #playerObject['facing'] = LEFT
+                elif event.key in (K_RIGHT, K_d):
+                    moveLeft = False
+                    moveRight = True
+                    #if playerObject['facing'] != RIGHT: # change player image
+                    #    playerObject['surface'] = pygame.transform.scale(PLAYERSURFACE1, (playerObject['size'], playerObject['size']))
+                    #playerObject['facing'] = RIGHT
+                elif winMode and event.key == K_r:
+                    return
 
             elif event.type == KEYUP:
                 # stop moving the player's squirrel
                 if event.key in (K_LEFT, K_a):
-                    playerObject['x'] = playerObject['x']
+                    moveLeft = False
                 elif event.key in (K_RIGHT, K_d):
-                   playerObject['x']=playerObject['x']
+                    moveRight = False
                 elif event.key in (K_UP, K_w):
-                    playerObject['y']=playerObject['y']
+                    moveUp = False
                 elif event.key in (K_DOWN, K_s):
-                    playerObject['y']=playerObject['y']
+                    moveDown = False
+
+                elif event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        if not gameOverMode:
+            # actually move the player
+            if moveLeft:
+                playerObject['x'] -= PLAYER_VELOCITY
+            if moveRight:
+                playerObject['x'] += PLAYER_VELOCITY
+            if moveUp:
+                playerObject['y'] -= PLAYER_VELOCITY
+            if moveDown:
+                playerObject['y'] += PLAYER_VELOCITY
+
+            for i in range(len(enemies) - 1, -1, -1):   #colliding function
+                eObj = enemies[i]
+                if 'rect' in eObj and playerObject['rect'].colliderect(eObj['rect']):
+                    # player/enemy collision has occurred
+
+                    if eObj['width'] * eObj['height'] <= playerObject['size'] ** 2:
+                        # player is larger and eats the squirrel
+                        playerObject['size'] += int((eObj['width'] * eObj['height']) ** 0.2 )+1
+                        del enemies[i]
+                        playerObject['surface']=pygame.transform.scale(PLAYERSURFACE,(playerObject['size'],playerObject['size']))
+
+                        if playerObject['size'] > WIN_SIZE:
+                            winMode = True  # turn on "win mode"
+
+                    elif not invulnerableMode:
+                            # player is smaller and takes damage
+                            invulnerableMode = True
+                            invulnerableStartTime = time.time()
+                            playerObject['health'] -= 1
+                            if playerObject['health'] == 0:
+                                gameOverMode = True  # turn on "game over mode"
+                                gameOverStartTime = time.time()
+
+        if winMode :
+            DISPLAYSURF.blit(winSurf,winRect)
+        elif gameOverMode:
+            DISPLAYSURF.blit(gameOverSurf,gameOverRect)
 
 
 
 
 
 
-        while len(stars)<NUM_STARS:
-            stars.append(makeNewStar(camx,camy))
-        while len(enemies) < NUM_ENEMY:         #jaise stars append kiye the wese hi enemies laa rahe h frame me
-            enemies.append(makeNewEnemies(camx, camy))
-
-        for i in range (len(stars)-1,0):
-            if stars[i]['x'] not in range(camx-SCREENWIDTH,camx+2*SCREENWIDTH) and stars[i]['y'] not in range(camy-SCREENHEIGHT,camy+2*SCREENHEIGHT):
-                del stars[i]
 
         pygame.display.update()
         CLOCK.tick(FPS)
@@ -261,16 +268,13 @@ def makeNewStar(camx,camy):
 def makeNewEnemies(camx, camy):    #yaha par enemies banaye
     eObj = {}
     generalSize = random.randint(5, 25)
-    multiplier = random.randint(1, 3)
-    eObj['width']  = (generalSize + random.randint(0, 10)) * multiplier
-    eObj['height'] = (generalSize + random.randint(0, 10)) * multiplier
+    eObj['width']  = (generalSize * random.randint(1, 3))
+    eObj['height'] = (generalSize * random.randint(1, 3))
     eObj['x'], eObj['y'] = cord_inactive(camx, camy, eObj['width'], eObj['height'])
     eObj['movex'] = getRandomVelocity()
     eObj['movey'] = getRandomVelocity()
-    if eObj['movex'] < 0: # squirrel is facing left
-        eObj['surface'] = pygame.transform.scale(PLAYERSURFACE, (eObj['width'], eObj['height']))
-    else: # squirrel is facing right
-        eObj['surface'] = pygame.transform.scale(PLAYERSURFACE1, (eObj['width'], eObj['height']))
+    eObj['surface'] = pygame.transform.scale(PLAYERSURFACE1, (eObj['width'], eObj['height']))
+
     return eObj
 
 def getRandomVelocity():
